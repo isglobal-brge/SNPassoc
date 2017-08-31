18dc83701b319a989ccb4afac31341a9b396dc0e
isglobal-brge <juanr.gonzalez@isglobal.org>
2017-08-31 15:27:23 +0200

    remove scorefreqs and use poisbinom

--- R/dscore.default.R
+++ R/dscore.default.R
@@ -3,8 +3,9 @@ dscore.default <- function(x, ...)
  {
    if(any(x>1 | x<0))
     stop("argument 'x' should be a vector containing probabilities")
-   ans <- .Call("scorefreqs", x, PACKAGE="SNPassoc")
-   names(ans) <- paste(0:(2*length(x)), "alleles")
+   v <- c(0:(2*length(x)))
+   ans <- dpoisbinom(v, rep(x, each=2))
+   names(ans) <- paste(v, "alleles")
    ans 
  }
 
