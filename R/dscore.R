ad3694fa4e4f85ac81ceb40b59a83bc65e056251
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2018-03-12 10:09:39 +0100

    dscore uploaded

--- R/dscore.R
+++ R/dscore.R
@@ -1,2 +1,10 @@
 dscore<-function(x,...)
  UseMethod("dscore")
+
+
+plot.dscore <- function(x, ...) {
+  xx <- gsub(" alleles", "", names(x))
+  plot(xx, x, type="h", xlab="risk alleles", 
+       ylab="exact probability", col="red", lwd=2)
+  
+}
--- R/dscore.default.R
+++ R/dscore.default.R
@@ -1,4 +1,25 @@
 
+dscore.character <- function(x, ...){
+  snpmart <- biomaRt::useMart("ENSEMBL_MART_SNP", 
+                              dataset = "hsapiens_snp", ...)
+  snpInfo <- biomaRt::getBM(c("refsnp_id", "chr_name", "chrom_start", 
+                     "allele", "minor_allele", "minor_allele_freq"),
+                   filters = c("snp_filter"),
+                   values = x, mart = snpmart)
+
+  if (nrow(snpInfo)!=length(x)) {
+    warning("Genetic score distribution has been computed with these SNPs: \n",
+            unique(snpInfo$refsnp_id))
+    
+  }
+    
+  mafs<-snpInfo$minor_allele_freq
+  ans <- dscore(mafs)
+  attr(ans, "MAFs") <- snpInfo
+  ans
+}
+
+
 dscore.default <- function(x, ...)
  {
    if(any(x>1 | x<0))
@@ -6,7 +27,7 @@ dscore.default <- function(x, ...)
    v <- c(0:(2*length(x)))
    ans <- dpoisbinom(v, rep(x, each=2))
    names(ans) <- paste(v, "alleles")
+   class(ans) <- "dscore"
    ans 
  }
-
 
