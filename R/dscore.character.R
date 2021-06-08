9961a7a8bd5cf7f3c781e926b1f77f44b1c01088
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2021-06-08 16:55:01 +0200

    function calls + typo corrected

--- R/dscore.character.R
+++ R/dscore.character.R
@@ -1,10 +1,10 @@
 dscore.character <- function(x, ...){
- rng <- snpsById(SNPlocs.Hsapiens.dbSNP144.GRCh38, ids=x)
+ rng <- BSgenome::snpsById(SNPlocs.Hsapiens.dbSNP144.GRCh38::SNPlocs.Hsapiens.dbSNP144.GRCh38, ids=x)
  genome(rng) <- "GRCh38"
  
- mafs <- gscores(MafDb.1Kgenomes.phase3.GRCh38, rng, pop=c("EUR_AF"))
+ mafs <- GenomicScores::gscores(MafDb.1Kgenomes.phase3.GRCh38::MafDb.1Kgenomes.phase3.GRCh38, rng, pop=c("EUR_AF"))
  
- fAlleles <- as.character(getSeq(BSgenome.Hsapiens.NCBI.GRCh38::Hsapiens, rng))
+ refAlleles <- as.character(getSeq(BSgenome.Hsapiens.NCBI.GRCh38::Hsapiens, rng))
  altAlleles <- DNA_BASES[(match(refAlleles, DNA_BASES)) %% 4 + 1]
 
  snpInfo <- data.frame(mafs, refAlleles, altAlleles) 
