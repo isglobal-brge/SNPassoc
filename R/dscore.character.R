ed7feb0a9613502e5d516db5347a82957d1b6731
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2022-12-02 16:46:53 +0100

    Update dscore.character.R
    changed genome to GRCh38.p2  -> and add 0.001 to missing SNPs

--- R/dscore.character.R
+++ R/dscore.character.R
@@ -1,13 +1,17 @@
 dscore.character <- function(x, ...){
  rng <- BSgenome::snpsById(SNPlocs.Hsapiens.dbSNP144.GRCh38::SNPlocs.Hsapiens.dbSNP144.GRCh38, ids=x)
- genome(rng) <- "GRCh38"
+ genome(rng) <- "GRCh38.p2"
  
  mafs <- GenomicScores::gscores(MafDb.1Kgenomes.phase3.GRCh38::MafDb.1Kgenomes.phase3.GRCh38, rng, pop=c("EUR_AF"))
  
- refAlleles <- as.character(getSeq(BSgenome.Hsapiens.NCBI.GRCh38::Hsapiens, rng))
+ gg <- BSgenome.Hsapiens.NCBI.GRCh38::Hsapiens
+ genome(gg) <- "GRCh38.p2"
+ 
+ refAlleles <- as.character(getSeq(gg, rng))
  altAlleles <- DNA_BASES[(match(refAlleles, DNA_BASES)) %% 4 + 1]
 
  snpInfo <- data.frame(mafs, refAlleles, altAlleles) 
+ snpInfo$EUR_AF[is.na(snpInfo$EUR_AF)] <- 0.001
  mafs <- snpInfo$EUR_AF
  ans <- dscore(mafs)
  attr(ans, "MAFs") <- snpInfo
