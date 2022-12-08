53169b7c7e538f147da987de8adc168848ea81a4
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2022-12-08 20:21:18 +0100

    removed dscore

--- R/dscore.setupSNP.R
+++
@@ -1,8 +0,0 @@
-dscore.setupSNP <- function(x, ...)
- {
-   ss <- summary(x, print=FALSE)
-   MAF <- 1 - (ss$major.allele.freq)/100
-   ans <- dscore(MAF)
-   attr(ans, "probs") <- MAF
-   ans   
- }
